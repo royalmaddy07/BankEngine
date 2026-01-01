6ea565f79156d71bf4ac712caac442890f0ccd7c
Yash Patel <therealyashpatel@gmail.com>
2026-01-01 22:55:06 +0530

    completed database and model connection

--- banking_wallet_system/base/admin.py
+++ banking_wallet_system/base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+# Registering the models created to the django admin
+admin.site.register(Users)
+admin.site.register(Transactions)
+admin.site.register(Accounts)
+admin.site.register(Ledgerentries)
+admin.site.register(Transactionstatus)
+admin.site.register(Auditlog)
--- banking_wallet_system/base/models.py
+++ banking_wallet_system/base/models.py
@@ -7,7 +7,7 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-
+# this is our Accounts table
 class Accounts(models.Model):
     accountid = models.BigAutoField(db_column='accountID', primary_key=True)  # Field name made lowercase.
     userid = models.ForeignKey('Users', models.DO_NOTHING, db_column='userID')  # Field name made lowercase.
@@ -15,39 +15,39 @@ class Accounts(models.Model):
     accounttype = models.CharField(db_column='accountType', max_length=10)  # Field name made lowercase.
     balance = models.DecimalField(max_digits=20, decimal_places=4)
     status = models.CharField(max_length=10)
-    createdat = models.DateTimeField(db_column='createdAt')  # Field name made lowercase.
+    createdate = models.DateTimeField(db_column='createdAt')  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'accounts'
 
-
+# this is our Auditlog table
 class Auditlog(models.Model):
     logid = models.BigAutoField(db_column='logID', primary_key=True)  # Field name made lowercase.
     entityname = models.CharField(db_column='entityName', max_length=50)  # Field name made lowercase.
     entityid = models.BigIntegerField(db_column='entityID')  # Field name made lowercase.
     action = models.CharField(max_length=50)
     performedby = models.BigIntegerField(db_column='performedBy')  # Field name made lowercase.
-    createdat = models.DateTimeField(db_column='createdAt')  # Field name made lowercase.
+    createdate = models.DateTimeField(db_column='createdAt')  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'auditlog'
 
-
+# this is our Ledgerentries table
 class Ledgerentries(models.Model):
     ledgerid = models.BigAutoField(db_column='ledgerID', primary_key=True)  # Field name made lowercase.
     transactionid = models.ForeignKey('Transactions', models.DO_NOTHING, db_column='transactionID')  # Field name made lowercase.
     accountid = models.ForeignKey(Accounts, models.DO_NOTHING, db_column='accountID')  # Field name made lowercase.
     entrytype = models.CharField(db_column='entryType', max_length=50)  # Field name made lowercase.
     amount = models.DecimalField(max_digits=20, decimal_places=5)
-    createdat = models.DateTimeField(db_column='createdAt')  # Field name made lowercase.
+    createdate = models.DateTimeField(db_column='createdAt')  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'ledgerentries'
 
-
+# this is our transactions table
 class Transactions(models.Model):
     transactionid = models.BigAutoField(db_column='transactionID', primary_key=True)  # Field name made lowercase.
     fromaccountid = models.ForeignKey(Accounts, models.DO_NOTHING, db_column='fromAccountID')  # Field name made lowercase.
@@ -55,13 +55,13 @@ class Transactions(models.Model):
     amount = models.DecimalField(max_digits=20, decimal_places=2)
     transactiontype = models.CharField(db_column='transactionType', max_length=10)  # Field name made lowercase.
     statusid = models.ForeignKey('Transactionstatus', models.DO_NOTHING, db_column='statusID')  # Field name made lowercase.
-    createdat = models.DateTimeField(db_column='createdAt')  # Field name made lowercase.
+    createdate = models.DateTimeField(db_column='createdAt')  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'transactions'
 
-
+# this is our Transactionstatus table
 class Transactionstatus(models.Model):
     statusid = models.AutoField(db_column='statusID', primary_key=True)  # Field name made lowercase.
     statusname = models.CharField(db_column='statusName', max_length=50)  # Field name made lowercase.
@@ -70,14 +70,14 @@ class Transactionstatus(models.Model):
         managed = False
         db_table = 'transactionstatus'
 
-
+# this is our Users table
 class Users(models.Model):
     userid = models.BigAutoField(db_column='userID', primary_key=True)  # Field name made lowercase.
     name = models.CharField(max_length=50)
     email = models.CharField(unique=True, max_length=50)
     phone = models.CharField(unique=True, max_length=10)
     status = models.CharField(max_length=10)
-    createdat = models.DateTimeField(db_column='createdAt')  # Field name made lowercase.
+    createdate = models.DateTimeField(db_column='createdAt')  # Field name made lowercase.
 
     class Meta:
         managed = False
